e98ce2cb44fc2e3b609c0069f6f1d3f6b14f9004
Stefan Möding <stm@kill-9.net>
2019-01-30 18:57:04 +0100

    Update to PDK version 1.9.0

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,10 +4,10 @@ describe 'gai' do
   context 'with default parameters' do
     it {
       is_expected.to contain_class('gai')
-        .with('label'      => {},
-              'precedence' => {},
-              'scopev4'    => {},
-              'reload'     => nil)
+        .with_label({})
+        .with_precedence({})
+        .with_scopev4({})
+        .with_reload(nil)
 
       is_expected.to contain_file('/etc/gai.conf')
         .with_ensure('file')
@@ -21,36 +21,59 @@ describe 'gai' do
     }
   end
 
-  context 'with valid parameter label' do
+  context 'with single parameter label' do
     let :params do
-      { label: { '::ffff:127.0.0.0.0' => '10' } }
+      { label: { '::ffff:127.0.0.0' => '10' } }
     end
 
     it {
       is_expected.to contain_file('/etc/gai.conf')
-        .with_content(%r{^label\s+::ffff:127.0.0.0.0\s+10$})
+        .with_content(%r{^label\s+::ffff:127.0.0.0\s+10$})
+    }
+  end
+  context 'with multiple parameter label' do
+    let :params do
+      { label: { '::1/128' => '0', '::/0' => '1' } }
+    end
+
+    it {
+      is_expected.to contain_file('/etc/gai.conf')
+        .with_content(%r{^label\s+::1/128\s+0$})
+        .with_content(%r{^label\s+::/0\s+1$})
     }
   end
 
-  context 'with valid parameter precedence' do
+  context 'with single parameter precedence' do
     let :params do
-      { precedence: { '::ffff:127.0.0.0.0' => '10' } }
+      { precedence: { '::ffff:127.0.0.0' => '10' } }
     end
 
     it {
       is_expected.to contain_file('/etc/gai.conf')
-        .with_content(%r{^precedence\s+::ffff:127.0.0.0.0\s+10$})
+        .with_content(%r{^precedence\s+::ffff:127.0.0.0\s+10$})
+    }
+  end
+
+  context 'with multiple parameter precedence' do
+    let :params do
+      { precedence: { '::1/128' => '50', '::/0' => '40' } }
+    end
+
+    it {
+      is_expected.to contain_file('/etc/gai.conf')
+        .with_content(%r{^precedence\s+::1/128\s+50$})
+        .with_content(%r{^precedence\s+::/0\s+40$})
     }
   end
 
   context 'with valid parameter scopev4' do
     let :params do
-      { scopev4: { '::ffff:127.0.0.0.0' => '10' } }
+      { scopev4: { '::ffff:127.0.0.0' => '10' } }
     end
 
     it {
       is_expected.to contain_file('/etc/gai.conf')
-        .with_content(%r{^scopev4\s+::ffff:127.0.0.0.0\s+10$})
+        .with_content(%r{^scopev4\s+::ffff:127.0.0.0\s+10$})
     }
   end
 
